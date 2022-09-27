27430464bf9097b8d2498a963448a52f096ed366
eggplantiny <eggplantiny@gmail.com>
2022-09-27 20:36:59 +0900

    test

--- src/app.js
+++ src/app.js
@@ -182,7 +182,31 @@ app.post('/api/', (req, res) => {
         },
       }
     ],
-    "url_private": 'https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512'
+    "attachments": [
+      {
+        "fallback": "Plain-text summary of the attachment.",
+        "color": "#2eb886",
+        "pretext": "Optional text that appears above the attachment block",
+        "author_name": "Bobby Tables",
+        "author_link": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
+        "author_icon": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
+        "title": "Slack API Documentation",
+        "title_link": "https://api.slack.com/",
+        "text": "Optional text that appears within the attachment",
+        "fields": [
+          {
+            "title": "Priority",
+            "value": "High",
+            "short": false
+          }
+        ],
+        "image_url": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
+        "thumb_url": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
+        "footer": "Slack API",
+        "footer_icon": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
+        "ts": 123456789
+      }
+    ]
   });
 });
 
