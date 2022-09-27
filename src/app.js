a8ed7a0ee06ec885c77408dd72e3994f9179a9a6
eggplantiny <eggplantiny@gmail.com>
2022-09-27 20:07:48 +0900

    good

--- src/app.js
+++ src/app.js
@@ -5,79 +5,117 @@ let app = express();
 const CORPUS = [
   {
     text: '귀엽고',
-    suffix: '귀여운'
+    suffix: '귀여운',
+    emotion: 'happiness',
+    emoji: '😍',
   },
   {
     text: '멋있고',
-    suffix: '멋진'
+    suffix: '멋진',
+    emotion: 'cool',
+    emoji: '😎',
   },
   {
     text: '잘생기고',
-    suffix: '잘생긴'
+    suffix: '잘생긴',
+    emotion: 'cool',
+    emoji: '😎',
   },
   {
     text: '머리좋고',
-    suffix: '머리좋은'
+    suffix: '머리좋은',
+    emotion: 'cool',
+    emoji: '🤓',
   },
   {
     text: '배고프고',
-    suffix: '배고픈'
+    suffix: '배고픈',
+    emotion: 'sadness',
+    emoji: '😩',
   },
   {
     text: '행복하고',
-    suffix: '행복한'
+    suffix: '행복한',
+    emotion: 'happiness',
+    emoji: '😊',
   },
   {
     text: '피곤하고',
-    suffix: '피곤한'
+    suffix: '피곤한',
+    emotion: 'sadness',
+    emoji: '😴',
   },
   {
     text: '배부르고',
-    suffix: '배부른'
+    suffix: '배부른',
+    emotion: 'happiness',
+    emoji: '😋',
   },
   {
     text: '졸리고',
-    suffix: '졸린'
-  },
-  {
-    text: '자신감 넘치고',
-    suffix: '자신감 넘치는'
-  },
-  {
-    text: '똑똑하고',
-    suffix: '똑똑한'
-  },
-  {
-    text: '귀여운',
-    suffix: '귀여운'
-  },
-  {
-    text: '섹시하고',
-    suffix: '섹시한'
-  },
-  {
-    text: '예쁘고',
-    suffix: '예쁜'
-  },
-  {
-    text: '세상에서 제일',
-    suffix: '세상에서 제일 멋진'
-  },
-  {
-    text: '우주에서 가장',
-    suffix: '우주에서 가장 멋진'
-  },
-  {
-    text: '지구에서 제일',
-    suffix: '지구에서 제일 멋진',
-  },
-  {
-    text: '이 세상에서 제일',
-    suffix: '이 세상에서 제일',
+    suffix: '졸린',
+    emotion: 'sadness',
+    emoji: '😴',
   },
   {
     text: '자신감 넘치고',
     suffix: '자신감 넘치는',
+    emotion: 'cool',
+    emoji: '😎',
+  },
+  {
+    text: '똑똑하고',
+    suffix: '똑똑한',
+    emotion: 'cool',
+    emoji: '🤓',
+  },
+  {
+    text: '귀여운',
+    suffix: '귀여운',
+    emotion: 'happiness',
+    emoji: '😍',
+  },
+  {
+    text: '섹시하고',
+    suffix: '섹시한',
+    emotion: 'cool',
+    emoji: '😘',
+  },
+  {
+    text: '예쁘고',
+    suffix: '예쁜',
+    emotion: 'happiness',
+    emoji: '😍',
+  },
+  {
+    text: '세상에서 제일',
+    suffix: '세상에서 제일 멋진',
+    emotion: 'cool',
+    emoji: '😎',
+  },
+  {
+    text: '우주에서 가장',
+    suffix: '우주에서 가장 멋진',
+    emotion: 'cool',
+    emoji: '😎',
+  },
+  {
+    text: '지구에서 제일',
+    suffix: '지구에서 제일 멋진',
+    emotion: 'cool',
+    emoji: '😎',
+  },
+  {
+    text: '이 세상에서 제일',
+    suffix: '이 세상에서 제일 멋진',
+    emotion: 'cool',
+    emoji: '😎',
+  },
+  {
+    text: '자신감 넘치고',
+    suffix: '자신감 넘치는',
+    emotion: 'cool',
+    emoji: '😎',
   }
 ]
 
@@ -100,11 +138,12 @@ function chooseRandomWords(n = 10) {
 function generateNickname(name = '김세진') {
   const corpus = chooseRandomWords();
   const words = []
-
+  let last = null;
   for (let index = 0; index < corpus.length; index += 1) {
-    console.log(index, corpus.length - 1)
+
     if (index === corpus.length - 1) {
       words.push(corpus[index].suffix);
+      last = corpus[index];
     }
     else {
       words.push(corpus[index].text);
@@ -112,6 +151,7 @@ function generateNickname(name = '김세진') {
   }
 
   words.push(name);
+  words.push(last.emoji);
 
   return words.join(' ')
 }
@@ -122,6 +162,7 @@ app.get('/api/', (req, res) => {
   res.json({
     "response_type": "in_channel",
     "text": nickname,
+    "url_private": 'https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512'
   });
 });
 
@@ -129,7 +170,8 @@ app.post('/api/', (req, res) => {
   const nickname = generateNickname()
   res.json({
     "response_type": "in_channel",
-    "text": nickname
+    "text": nickname,
+    "url_private": 'https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512'
   });
 });
 
