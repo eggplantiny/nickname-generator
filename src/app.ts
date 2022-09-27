f351cd5e169f63d799552abbcfb6ccaddedc58ae
sungwoo.jung@kt.com <eggplantiny@gmail.com>
2022-09-27 17:50:18 +0900

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+
+let app = express();
+
+const level1 = ['귀엽고', '멋있고', '예쁘고', '좋은', '머리좋고', '똑똑하고', '잘생기고', '매력적인', '귀여운', '멋진', '예쁜', '좋은', '머리좋은', '똑똑한', '잘생긴', '매력적인'];
+const level2 = ['똑똑한', '잘생긴', '매력적인', '귀여운', '멋진', '예쁜', '좋은', '머리좋은', '똑똑한', '잘생긴', '매력적인'];
+
+app.post('/api/', (req, res) => {
+  const l1 = level1[Math.floor(Math.random() * level1.length)];
+  const l2 = level2[Math.floor(Math.random() * level2.length)];
+  res.send(`${l1} ${l2} 갓세진`);
+});
+
+app.listen(10047);
