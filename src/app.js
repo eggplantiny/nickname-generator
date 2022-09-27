8a41b8e9fa331c6d5e35912a4b622b901da9cfdd
eggplantiny <eggplantiny@gmail.com>
2022-09-27 22:04:35 +0900

    good

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
+const DEFAULT_IMAGE = 'https://my-lingo.com/image/I1290.png'
+const DEFAULT_COLOR = '#2eb886';
+
 const CORPUS = [
   {
     text: '귀엽고',
@@ -116,6 +119,14 @@ const CORPUS = [
   }
 ]
 
+const UserInfo = {
+  '김세진': {
+    image: 'https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512',
+    link: 'https://www.naver.com',
+    color: '#2eb886',
+  }
+}
+
 // choose random number of words from corpus with no conflicts
 function chooseRandomWords(n = 10) {
   let words = [];
@@ -153,6 +164,41 @@ function generateNickname(name = '김세진') {
   return words.join(' ')
 }
 
+function hasCard(name) {
+  return name in UserInfo;
+}
+
+function generateCard(name, nickname, userName) {
+  return {
+    "response_type": "in_channel",
+    "attachments": [
+      {
+        "fallback": "Plain-text summary of the attachment.",
+        "color": UserInfo[name]?.color ?? DEFAULT_COLOR,
+        "pretext": `${name} 님의 닉네임은?`,
+        "author_name": userName,
+        "author_link": UserInfo[name]?.image ?? DEFAULT_IMAGE,
+        "author_icon": UserInfo[name]?.image ?? DEFAULT_IMAGE,
+        "title": `${name} 님의 닉네임은?`,
+        "title_link": UserInfo[name]?.link ?? DEFAULT_IMAGE,
+        "text": nickname,
+        "fields": [
+          // {
+          //   "title": "Priority",
+          //   "value": "High",
+          //   "short": false
+          // }
+        ],
+        "image_url": UserInfo[name]?.image ?? DEFAULT_IMAGE,
+        "thumb_url": UserInfo[name]?.image ?? DEFAULT_IMAGE,
+        "footer": `${name} 님의 사진`,
+        "footer_icon": UserInfo[name]?.image ?? DEFAULT_IMAGE,
+        "ts": Date.now()
+      }
+    ]
+  }
+}
+
 app.get('/api/', (req, res) => {
   const nickname = generateNickname()
 
@@ -171,45 +217,7 @@ app.post('/api/', (req, res) => {
   const nickname = generateNickname(name)
 
   res.setHeader('content-type', 'application/json');
-
-  res.json({
-    "response_type": "in_channel",
-    "blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `*${nickname}*`
-        },
-      }
-    ],
-    "attachments": [
-      name === '김세진' ?       {
-        "fallback": "Plain-text summary of the attachment.",
-        "color": "#2eb886",
-        "pretext": `${name} 님의 닉네임은?`,
-        "author_name": userName,
-        "author_link": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
-        "author_icon": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
-        "title": `${name} 님의 닉네임은?`,
-        "title_link": "https://api.slack.com/",
-        "text": nickname,
-        "fields": [
-          // {
-          //   "title": "Priority",
-          //   "value": "High",
-          //   "short": false
-          // }
-        ],
-        "image_url": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
-        "thumb_url": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
-        "footer": `${name} 님의 사진`,
-        "footer_icon": "https://ca.slack-edge.com/T03B3BN98DC-U03CAPJ175F-a1ecf5beed3e-512",
-        "ts": 123456789
-      }
-      : null
-    ].filter(x => x)
-  });
+  res.json(generateCard(name, nickname, userName));
 });
 
 app.listen(10054);
