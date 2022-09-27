f261483f6cb8af3f77533cb8ec4b250e456a9c4c
sungwoo.jung@kt.com <eggplantiny@gmail.com>
2022-09-27 18:27:24 +0900

    good

--- src/app.js
+++ src/app.js
@@ -5,10 +5,21 @@ let app = express();
 const level1 = ['귀엽고', '멋있고', '예쁘고', '좋은', '머리좋고', '똑똑하고', '잘생기고', '매력적고', '멋지고', '자신감 넘치고', '세상에서 제일', '우주에서 가장', '이 세상에서 최고로'];
 const level2 = ['똑똑한', '잘생긴', '매력적인', '귀여운', '멋진', '예쁜', '좋은', '머리좋은', '똑똑한', '잘생긴', '매력적인'];
 
-app.get('/api/', (req, res) => {
+function generateNickname(name = '김세진') {
   const l1 = level1[Math.floor(Math.random() * level1.length)];
   const l2 = level2[Math.floor(Math.random() * level2.length)];
-  res.send(`${l1} ${l2} 김세진`);
+
+  return `${l1} ${l2} ${name}`;
+}
+
+app.get('/api/', (req, res) => {
+  const nickname = generateNickname()
+  res.send(nickname);
 });
+
+app.post('/api/', (req, res) => {
+  const nickname = generateNickname()
+  res.send(nickname);
+}
 
 app.listen(10054);
